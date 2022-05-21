af6fffce71e1e39f477d49b441a057c93bc8f44a
ozgur-demir <info@csyolu.com>
2022-05-21 15:58:05 +0300

    Multi-Controller example added

---
+++ RateLimit.API/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            return Ok(new { Id = 1, Name = "Items"});
+        }
+        [HttpPost]
+        public IActionResult SaveProduct()
+        {
+            return Ok(new { Status = "Success" });
+        }
+    }
+}
