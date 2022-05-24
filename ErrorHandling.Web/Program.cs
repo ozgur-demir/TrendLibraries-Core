fe8d6d3c2f7f351ee75277ba4b5035d93d712243
ozgur-demir <info@csyolu.com>
2022-05-25 01:58:04 +0300

    Error handling via Status Code

--- ErrorHandling.Web/Program.cs
+++ ErrorHandling.Web/Program.cs
@@ -4,12 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-app.UseExceptionHandler("/Home/Error");
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-  
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //app.UseStatusCodePages("text/plain","An error occured.Status code: {0}"); //method1 for httpstatuscode error handling
+    app.UseStatusCodePages(async context =>
+    {
+        context.HttpContext.Response.ContentType = "text/plain";
+        await context.HttpContext.Response.WriteAsync($"An error occured.Status Code:{context.HttpContext.Response.StatusCode}");//method2  for httpstatuscode error handling
+    });
     app.UseHsts();
 }
 
