bc8cd7b4a2c263092e655fe2627db1c130dfeac4
ozgur-demir <info@csyolu.com>
2022-05-30 17:06:42 +0300

    API controller created via EF Database First

---
+++ Swagger.API/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swagger.API.Models;
+using Swashbuckle.Swagger;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(gen =>
+{
+    gen.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Product API",
+        Description = "An ASP.NET Core Web API for managing Product items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Ozgur Demir",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName()}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    gen.IncludeXmlComments(xmlPath);
+});
+builder.Services.AddDbContext<SwaggerDBContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SwaggerDB"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
